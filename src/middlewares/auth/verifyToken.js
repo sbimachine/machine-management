7081b4bef88fa6ec2dd1ff8b843078eab0f8f251
sbimachine <bsimachinemanagement@outlook.com>
2024-03-19 19:54:58 +0700

    fix: deployment cannot find module

--- src/middlewares/auth/verifyToken.js
+++ src/middlewares/auth/verifyToken.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const CustomError = require('../../utils/CustomError');
-const asyncErrorhandler = require('../../controllers/asyncErrorhandler');
+const asyncErrorHandler = require('../../controllers/asyncErrorHandler');
 
-const verifyToken = asyncErrorhandler(async (req, res, next) => {
+const verifyToken = asyncErrorHandler(async (req, res, next) => {
   if (!req.headers.authorization) return next(new CustomError('no token provided', 401));
 
   const token = req.headers.authorization.split(' ').filter((value) => value !== 'Bearer')[0];
