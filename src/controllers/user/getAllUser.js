870d7c4917b36a875651660266aeab80540d0b56
sbimachine <bsimachinemanagement@outlook.com>
2024-05-20 14:33:23 +0700

    feat: filter by name

--- src/controllers/user/getAllUser.js
+++ src/controllers/user/getAllUser.js
@@ -4,15 +4,23 @@ const getPagination = require('../../utils/getPagination');
 const sendResponse = require('../../utils/sendResponse');
 
 const getAllUserController = asyncErrorHandler(async (req, res, next) => {
-  const { firstName, lastName, role, page = 1, limit = 10, sort, sortDirection = 'DESC' } = req.query;
+  const { firstName, lastName, role, page = 1, limit = 10, sort, sortDirection = 'DESC', name } = req.query;
   let sortBy = [['createdAt', sortDirection]];
   if (sort === 'firstName') sortBy = [['firstName', sortDirection]];
   if (sort === 'lastName') sortBy = [['lastName', sortDirection]];
   if (sort === 'role') sortBy = [['role', sortDirection]];
   const offset = (parseInt(page) - 1) * parseInt(limit || 10);
-
+  const { Op } = Sequelize;
   const conditions = {
     ...(role ? { role } : {}),
+    ...(name
+      ? {
+          [Op.or]: [
+            { firstName: { [Op.iLike]: `%${name.toLowerCase()}%` } },
+            { lastName: { [Op.iLike]: `%${name.toLowerCase()}%` } },
+          ],
+        }
+      : {}),
     ...(firstName ? { firstName: { [Sequelize.Op.iLike]: `%${firstName.toLowerCase()}%` } } : {}),
     ...(lastName ? { lastName: { [Sequelize.Op.iLike]: `%${lastName.toLowerCase()}%` } } : {}),
   };
