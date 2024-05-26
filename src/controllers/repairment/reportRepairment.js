5584dd4ef0c0d98a1747cdc57febd1ccdc95733c
sbimachine <bsimachinemanagement@outlook.com>
2024-05-27 01:46:48 +0700

    fix: reported still false when done uploading image

--- src/controllers/repairment/reportRepairment.js
+++ src/controllers/repairment/reportRepairment.js
@@ -1,20 +1,26 @@
 const asyncErrorHandler = require('../asyncErrorHandler');
 const sendResponse = require('../../utils/sendResponse');
-const { RepairmentImage, Repairment } = require('../../models');
+const { RepairmentImage, Repairment, sequelize } = require('../../models');
 const CustomError = require('../../utils/CustomError');
 
-const reportRepairmentController = asyncErrorHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const { images, userPayload } = req;
-  const repairment = await Repairment.findOne({ where: { id, userId: userPayload.id } });
-  if (!repairment) return next(new CustomError('Data not found', 404));
+const reportRepairmentController = async (req, res, next) => {
+  const t = await sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const { images, userPayload } = req;
+    const repairment = await Repairment.findOne({ where: { id, userId: userPayload.id } });
+    if (!repairment) return next(new CustomError('Data not found', 404));
 
-  const imageItems = images.map((imageUrl) => ({ imageUrl, repairmentId: id }));
-  //   const repairmentImages = await RepairmentImage.findAll();
-  //   await repairment.createImages(imageItems);
-  const repairmentImages = await RepairmentImage.bulkCreate(imageItems);
-
-  sendResponse(res, 'repairment reported', repairmentImages);
-});
+    const imageItems = images.map((imageUrl) => ({ imageUrl, repairmentId: id }));
+    const repairmentImages = await RepairmentImage.bulkCreate(imageItems, { transaction: t });
+    await Repairment.update({ isReported: true }, { where: { id, userId: userPayload.id }, transaction: t });
+    await t.commit();
+    sendResponse(res, 'repairment reported', repairmentImages);
+  } catch (error) {
+    console.log(error);
+    await t.rollback();
+    next(error);
+  }
+};
 
 module.exports = reportRepairmentController;
